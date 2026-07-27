86cf4a977c2c8c4dc6497b74db753a28c884623c
iki1uc <entertrainer@web.de>
2026-07-27 05:10:01 +0200

    Update ZONE.conflict.js

--- ZONE/ZONE.conflict.js
+++ ZONE/ZONE.conflict.js
@@ -1,1 +1,6 @@
+export function ZoneConflict(Q, X) {
+    if (X.priority > Q.priority) return "X81 gewinnt";
+    if (Q.priority > X.priority) return "Q81 gewinnt";
+    return "Gleichstand – Meta entscheidet";
+}
 
